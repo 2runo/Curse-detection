fb3c7d83035211bdd81f0c6b9414080b390fb91d
2runo <61267275+2runo@users.noreply.github.com>
2020-04-30 03:35:27 +0900

    Update curse_detector.py

--- src/curse_detector.py
+++ src/curse_detector.py
@@ -11,7 +11,10 @@ class CurseDetector():
 
     def load_weights(self, path):
         # 학습된 weights 가져오기
-        self.model.load_weights(path)
+        try:
+            self.model.load_weights(path)
+        except OSError:
+            raise Exception("학습된 모델을 불러오는 데 실패했습니다. 학습된 모델(weights.h5)을 models 폴더에 옯겨 주세요.")
 
     def embedding(self, texts):
         # 전처리, 임베딩 수행
@@ -36,18 +39,19 @@ class CurseDetector():
         return pred
 
 
-curse = CurseDetector()
+if __name__ == "__main__":
+    curse = CurseDetector()
 
-print(curse.predict('씨발'))    # [0.0023 0.9976]
-print(curse.predict('ㅆ발'))    # [0.0159 0.9840]
-print(curse.predict('^^ㅣ발'))  # [0.0570 0.9429]
-print(curse.predict(['이게 뭔,,, 개소리야... 아오...',
-                     '전자발찌도 차야 함..',
-                     '부정적평가 하는 씨발것들은 머죠??',
-                     '보는내내  너무 화가났었어요.  소시오패스같아요.',
-                     '아침부터 시야흐려지네.ㅠㅠ']))
-# [[0.0043 0.9956]
-#  [0.7492 0.2507]
-#  [0.0102 0.9897]
-#  [0.9164 0.0835]
-#  [0.7681 0.2318]]
+    print(curse.predict('씨발'))    # [0.0023 0.9976]
+    print(curse.predict('ㅆ발'))    # [0.0159 0.9840]
+    print(curse.predict('^^ㅣ발'))  # [0.0570 0.9429]
+    print(curse.predict(['이게 뭔,,, 개소리야... 아오...',
+                         '전자발찌도 차야 함..',
+                         '부정적평가 하는 씨발것들은 머죠??',
+                         '보는내내  너무 화가났었어요.  소시오패스같아요.',
+                         '아침부터 시야흐려지네.ㅠㅠ']))
+    # [[0.0043 0.9956]
+    #  [0.7492 0.2507]
+    #  [0.0102 0.9897]
+    #  [0.9164 0.0835]
+    #  [0.7681 0.2318]]
